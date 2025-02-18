7a08433ef75be7224bc3c3eef0cccb36cd6f31d0
SayanDasDev <mesayan19@gmail.com>
2025-02-18 15:07:03 +0530

    hero page done

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,15 +1,36 @@
-"use client";
-import { useEffect, useState } from "react";
+import { HeroSection } from "@/components/hero-section";
+import { Icons } from "@/components/ui/icons";
 
 export default function Home() {
-  const [message, setMessage] = useState("Loading...");
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:5000/users")
-      .then((response) => response.json())
-      .then((data) => setMessage(data.message))
-      .catch(() => setMessage("Error fetching data"));
-  }, []);
-
-  return <div>{message}</div>;
+  return (
+    <HeroSection
+      badge={{
+        text: "Introducing our new models",
+        action: {
+          text: "Learn more",
+          href: "/docs",
+        },
+      }}
+      title="My AI Chat"
+      description="Join us to experience the most advanced AI chat platform that connects you with intelligent conversations and personalized interactions."
+      actions={[
+        {
+          text: "Get Started",
+          href: "/login",
+          variant: "default",
+        },
+        {
+          text: "GitHub",
+          href: "https://github.com/my_ai_chat",
+          variant: "secondary",
+          icon: <Icons.gitHub className="h-5 w-5" />,
+        },
+      ]}
+      image={{
+        light: "/chatui.webp",
+        dark: "/chatui.webp",
+        alt: "UI Preview",
+      }}
+    />
+  );
 }
